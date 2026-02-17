065961528ba281ea183f822bc5bba66e87fab4ef
Honza468 <janjpichl@gmail.com>
2026-02-17 09:37:26 +0100

    Initial - hopefully not last - commit

---
+++ prvni_program.py
@@ -0,0 +1,6 @@
+#This is a comment
+#This code was written in VIM, in Graz (AT)
+#17. 2. 2026
+print("Hello, world") #Only prints this to terminal
+name = input("Enter your name: ")
+print("Hello,", name," Welcome!")
