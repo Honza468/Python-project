27d074e97cdf93d0e635c377baa2d94daf1924db
Honza468 <janjpichl@gmail.com>
2026-02-19 19:07:54 +0100

    Type functions added

--- prvni_program.py
+++ prvni_program.py
@@ -29,4 +29,32 @@ print("Welcome,",name2,"!" )
 print("You are currently in:",location2)
 print("You are now",age2,"years old!")
 
+#---------------------------------------
+name3 = input("Enter your name here: ")
+print("You are welcomed here,",name3)
+age3 = int(input("Enter your age here: "))
+print("You are",age3,"old!")
+weight = float(input("Please, enter your weight here: "))
+print("You weigh",weight)
+weight2 = input("Enter if in kilograms or pounds: ")
+print("You weigh",weight,weight2)
+
+#--------------------------------------
+print(type(name))
+print(type(age))
+print(type(location))
+
+print(type(x))
+print(type(y))
+print(type(namex))
+print(type(namey))
+
+print(type(name2))
+print(type(location2))
+print(type(age2))
+
+print(type(name3))
+print(type(age3))
+print(type(weight))
+print(type(weight2))
 
